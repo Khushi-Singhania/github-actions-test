0d7f276c7ce6be80152eeca81111cfe6a5973c19
Khushi <khushi@Khushis-MacBook-Pro.local>
2023-04-27 14:03:32 +0530

    New App'

--- app.js
+++ app.js
@@ -1,11 +1,3 @@
-const express= require('express')
-const app= express();
-//create a simple route when 'name' is accessed and some data is sent to whoever is querying it.
-app.get('/names',(req,res)=>{   
-    res.send("Khushi"); // when app.get() is used then a name=Khushi is sent to that person
-})
-
-//The app is run on port:5000 using app.listen()
-app.listen(5000, () =>{
-    console.log("Server is runnong on port: 5000");
-} )
+console.log('Hello, Demo!');
+const d= new Date();
+console.log(d);
